10c9b7157e00b210cbf030bbc8646e1546b34411
tokhanh224 <tokhanh224@gmail.com>
2023-12-02 13:01:45 +0700

    update

--- controller/UserController.php
+++ controller/UserController.php
@@ -4,7 +4,7 @@ class UserController
     public function index()
     {
         if (isset($_COOKIE["role"])) {
-            if ($_COOKIE['role'] == 1) {
+            if ($_COOKIE['role'] != 0) {
                 include('view/user/useradmin.php');
             } else {
                 include 'view/user/user.php';
